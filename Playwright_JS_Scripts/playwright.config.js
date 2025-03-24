a20f554565510a77ffc2e70e8a81795ba8b189eb
Rajnish Parmar <rajnishparmar@Rajnishs-MacBook-Air.local>
2025-03-24 12:21:53 -0400

    trace value change

--- Playwright_JS_Scripts/playwright.config.js
+++ Playwright_JS_Scripts/playwright.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retry-with-trace',
+    trace: 'retain-on-failure',
     video: 'on'
   },
 
